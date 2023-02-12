a3d2edf938845cd32489a7a7acfdbe505559df41
Ivan <idruizbe@gmail.com>
2023-02-12 02:39:25 -0500

    Inicio del proyecto

---
+++ src/main/java/com/sofka/sofkaU/qa/consultorio/consultorio/ConsultorioApplication.java
@@ -0,0 +1,10 @@
+package com.sofka.sofkaU.qa.consultorio.consultorio;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ConsultorioApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioApplication.class, args);
+	}
+
+}
